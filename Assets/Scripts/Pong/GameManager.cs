206c8f294c1e47072adb22884001ff022052eb56
chavezfk <chavezfk@gmail.com>
2020-12-12 15:32:24 -0700

    removed unnecessary includes.

--- Assets/Scripts/Pong/GameManager.cs
+++ Assets/Scripts/Pong/GameManager.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using TMPro;
-using UnityEditor.iOS;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
