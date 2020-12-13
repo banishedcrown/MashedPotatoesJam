910a625c00aaf4ca679dca476e404685aeb77819
ibrowmew <ibrowmew@gmail.com>
2020-12-13 16:47:00 -0700

    Upgrades.

---
+++ Assets/Scripts/UpgradesScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradesScript : MonoBehaviour
+{
+    public GameObject[] Prompt;
+
+    void Start()
+    {
+        GameManager m = GameManager.GetManager();
+        ProgressData p = m.GetData().progress;
+
+        int core = p.coreLevel;
+        int bits = p.bitLevel;
+
+        int num = 0;
+        Prompt[num].SetActive(true);
+    }
+
+    void CoreIncrease(int level)
+    {
+        GameManager.GetManager().GetData().progress.AddCoreLevel(level);
+    }
+
+    void BitsIncrease(int level)
+    {
+        GameManager.GetManager().GetData().progress.AddBitLevel(level);
+    }
+
+}
