604f338c7078673e1bac8a7f15da07c0f1aca43b
ibrowmew <ibrowmew@gmail.com>
2020-12-13 17:38:43 -0700

    Yay

--- Assets/Scripts/UpgradesScript.cs
+++ Assets/Scripts/UpgradesScript.cs
@@ -15,15 +15,54 @@ public class UpgradesScript : MonoBehaviour
         int bits = p.bitLevel;
 
         int num = 0;
+        int bitandcore = bits * core;
+
+        if ((bitandcore) == 0)
+        {
+            if ((bits + core) == 1)
+            {
+                num = 0;
+            }
+            else
+            {
+                num = 1;
+            }
+        }
+        else if ((bitandcore) == 1)
+        {
+            num = 2;
+        }
+        else if ((bitandcore) == 2)
+        {
+            num = 3;
+        }
+        else if ((bitandcore) > 2 && (bitandcore) <= 4 )
+        {
+            num = 4;
+        }
+        else if ((bitandcore) > 4 && (bitandcore) <= 6)
+        {
+            num = 5;
+        }
+        else if ((bitandcore) > 6 && (bitandcore) <= 8)
+        {
+            num = 6;
+        }
+        else if ((bitandcore) > 8)
+        {
+            num = 7;
+        }
+
         Prompt[num].SetActive(true);
+
     }
 
-    void CoreIncrease(int level)
+    public void CoreIncrease(int level)
     {
         GameManager.GetManager().GetData().progress.AddCoreLevel(level);
     }
 
-    void BitsIncrease(int level)
+    public void BitsIncrease(int level)
     {
         GameManager.GetManager().GetData().progress.AddBitLevel(level);
     }
