33aab1191aa16970c4f83b15d373b0f9ae2402b3
ibrowmew <ibrowmew@gmail.com>
2020-12-20 21:27:09 -0700

    Updates to the upgrades. Not quite done but want to push what I have.

--- Assets/Scripts/UpgradesScript.cs
+++ Assets/Scripts/UpgradesScript.cs
@@ -9,48 +9,97 @@ public class UpgradesScript : MonoBehaviour
     void Start()
     {
         GameManager m = GameManager.GetManager();
-        ProgressData p = m.GetData().progress;
+        var p = m.GetData().progress;
 
         int core = p.coreLevel;
         int bits = p.bitLevel;
 
-        int num = 0;
-        int bitandcore = bits * core;
+        /*Defines the point in the array of gameobjects the game takes, 0 is errors, 1 is the start, 2 is the 2nd, 3 is when you can still gain both cores 
+        and bytes, 4 is when you can upgrade only cores, 5 is when you can upgrade only bytes, 6 is balanced victory, 7 is byte favored victory,
+        and 8 is core favored victory, and 9 is the final victory*/
+        int num;
+        //Bitandcore is formatted so that bitlevel takes the 10's place and corelevel takes the 1's place.
+        int bitandcore = bits * 10 + core;
 
-        if ((bitandcore) == 0)
+
+        switch (bitandcore)
         {
-            if ((bits + core) == 0)
-            {
-                num = 0;
-            }
-            else
-            {
+            case 44:
+                num = 9;
+                return;
+
+            case 43:
+                num = 7;
+                break;
+
+            case 42:
+                num = 4;
+                break;
+
+            case 41:
+                num = 4;
+                break;
+
+            case 34:
+                num = 8;
+                break;
+
+            case 33:
+                num = 6;
+                break;
+
+            case 32:
+                num = 3;
+                break;
+
+            case 31:
+                num = 3;
+                break;
+
+            case 24:
+                num = 5;
+                break;
+
+            case 23:
+                num = 3;
+                break;
+
+            case 22:
+                num = 3;
+                break;
+
+            case 21:
+                num = 3;
+                break;
+
+            case 14:
+                num = 5;
+                break;
+
+            case 13:
+                num = 3;
+                break;
+
+            case 12:
+                num = 3;
+                break;
+
+            case 11:
+                num = 3;
+                break;
+
+            case 10:
+                num = 2;
+                break;
+
+            case 0:
                 num = 1;
-            }
-        }
-        else if ((bitandcore) == 1)
-        {
-            num = 2;
-        }
-        else if ((bitandcore) == 2)
-        {
-            num = 3;
-        }
-        else if ((bitandcore) > 2 && (bitandcore) <= 4 )
-        {
-            num = 4;
-        }
-        else if ((bitandcore) > 4 && (bitandcore) <= 6)
-        {
-            num = 5;
-        }
-        else if ((bitandcore) > 6 && (bitandcore) <= 8)
-        {
-            num = 6;
-        }
-        else if ((bitandcore) > 8)
-        {
-            num = 7;
+                break;
+
+            default:
+                num = 0;
+                break;
+
         }
 
         Prompt[num].SetActive(true);
@@ -60,6 +109,11 @@ public class UpgradesScript : MonoBehaviour
     public void CoreIncrease(int level)
     {
         GameManager.GetManager().GetData().progress.AddCoreLevel(level);
+        
+        GameManager m = GameManager.GetManager();
+
+        m.GetData().currentPB /= 2;
+
     }
 
     public void BitsIncrease(int level)
