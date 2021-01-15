5b20e7a42a3cf4a5c616e2161b763baecc30472b
mewbusi <mewbusi@gmail.com>
2021-01-14 21:10:22 -0700

    Work on Main Menu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Version;
+    public GameObject Platform;
+
     public void Quit()
     {
         Application.Quit();
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+        Debug.Log(Application.platform);
+        Debug.Log(Application.version);
+
+        if (Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            Platform.GetComponent<TMP_Text>().text = "Windows";
+        }
+        else if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            Platform.GetComponent<TMP_Text>().text = "Web";
+        }
+        else if (Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            Platform.GetComponent<TMP_Text>().text = "Mac";
+        }
+        else if (Application.platform == RuntimePlatform.LinuxPlayer)
+        {
+            Platform.GetComponent<TMP_Text>().text = "Linux";
+        }
+        else
+        {
+            Platform.GetComponent<TMP_Text>().text = " ";
+        }
+
+        Version.GetComponent<TMP_Text>().text = "V" + Application.version;
+
     }
 }
