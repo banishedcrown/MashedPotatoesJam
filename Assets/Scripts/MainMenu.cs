3bff5f1251decd5c02dad6a2b8ba1921f2411ae6
mewbusi <mewbusi@gmail.com>
2021-01-16 14:26:06 -0700

    Main menu script update.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject Version;
     public GameObject Platform;
+    public GameObject MenuPassword;
+    public GameObject MenuNoPassword;
 
     public void Quit()
     {
@@ -16,31 +18,44 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Start");
-        Debug.Log(Application.platform);
-        Debug.Log(Application.version);
+        //Debug.Log(Application.platform);
+        //Debug.Log(Application.version);
 
+        //Check the runtime platform, output the platform into the platform gameobject, then set the 2 gameobjects with menu elements to true or false.
         if (Application.platform == RuntimePlatform.WindowsPlayer)
         {
             Platform.GetComponent<TMP_Text>().text = "Windows";
+            MenuPassword.SetActive(false);
+            MenuNoPassword.SetActive(true);
         }
         else if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
             Platform.GetComponent<TMP_Text>().text = "Web";
+            MenuPassword.SetActive(true);
+            MenuNoPassword.SetActive(false);
         }
         else if (Application.platform == RuntimePlatform.OSXPlayer)
         {
             Platform.GetComponent<TMP_Text>().text = "Mac";
+            MenuPassword.SetActive(false);
+            MenuNoPassword.SetActive(true);
         }
         else if (Application.platform == RuntimePlatform.LinuxPlayer)
         {
             Platform.GetComponent<TMP_Text>().text = "Linux";
+            MenuPassword.SetActive(false);
+            MenuNoPassword.SetActive(true);
         }
         else
         {
-            Platform.GetComponent<TMP_Text>().text = " ";
+            //If all checks fail just push the application platform unformatting and set to the windows version of the main menu.
+            Platform.GetComponent<TMP_Text>().text = Application.platform.ToString();
+            MenuPassword.SetActive(false);
+            MenuNoPassword.SetActive(true);
         }
 
+
+        //Then set the application version text to the current version.
         Version.GetComponent<TMP_Text>().text = "V" + Application.version;
 
     }
