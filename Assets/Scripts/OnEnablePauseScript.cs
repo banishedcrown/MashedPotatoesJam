f362e9db1b0676f76c63ead75c05a08017490d65
mewbusi <mewbusi@gmail.com>
2021-01-17 17:32:56 -0700

    Bug Cleanup and General Formatting.

---
+++ Assets/Scripts/OnEnablePauseScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnablePauseScript : MonoBehaviour
+{
+    float PriorTimeScale;
+
+    void OnEnable()
+    {
+        Pause();
+    }
+
+    void Pause()
+    {
+        PriorTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+        Debug.Log(Time.timeScale);
+    }
+
+    public void Resume()
+    {
+        if (PriorTimeScale == 1)
+        {
+            Time.timeScale = 1;
+        }
+
+        //Otherwise do Nothing
+
+    }
+}
---
+++ Assets/Scripts/OptionsOffBandaid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsOffBandaid : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        GameObject g = GameObject.Find("Options Panel");
+        if (g != null)
+            g.SetActive(false);
+    }
+}
